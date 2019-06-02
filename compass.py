135d46f00b1090746bcf797b64c57ceb857613d7
Harrison <harrison@Harrisons-MacBook-Pro.local>
2019-06-01 20:11:27 -0500

    init and prob final commit

---
+++ compass.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from PIL import Image
+from PIL import Image
+import argparse
+import sys
+
+image_scale = 0.075
+
+def place(bg, lang, econ, auth):
+	wd, ht = bg.size
+	lang_img = Image.open(lang)
+	lang_img.thumbnail((int(image_scale*wd), int(image_scale*wd)), Image.ANTIALIAS)
+	bg.paste(lang_img, (int(wd*econ), int(ht*(1-auth))), lang_img)
+
+parser = argparse.ArgumentParser(description="Become a poly sci major with one CLI")
+parser.add_argument("-s", type = float, help = "scale at which icons should appear w.r.t. width", default = 0.1)
+parser.add_argument("-b", type = str, help = "background image", default = "compass.png")
+args = parser.parse_args()
+image_scale = args.s
+bg = args.b
+
+background = Image.open(args.b)
+for lang in sys.stdin:
+	econ, auth, name = lang.split(' ')
+	place(background, name.strip(), float(econ), float(auth))
+background.show()
